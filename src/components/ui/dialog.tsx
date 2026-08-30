3021dba330be6a726f53bf1e3ce3b2ad505da688
wojr <110169607+juniornomada@users.noreply.github.com>
2026-08-30 17:22:22 -0300

    fix: make transaction editor truly responsive on mobile

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -50,7 +50,9 @@ const DialogContent = React.forwardRef<
           responsiveMobileSheet
             ? "fixed inset-x-0 bottom-0 z-50 grid w-full max-w-none translate-x-0 translate-y-0 gap-4 border bg-background p-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 sm:left-[50%] sm:top-[50%] sm:bottom-auto sm:w-full sm:max-w-lg sm:translate-x-[-50%] sm:translate-y-[-50%] sm:rounded-lg sm:p-6"
             : "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-4 sm:p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-          className
+          className,
+          legacyMobileEditor &&
+            "!fixed !left-0 !right-0 !top-0 !bottom-auto !h-[100dvh] !max-h-[100dvh] !w-screen !max-w-none !translate-x-0 !translate-y-0 !rounded-none !overflow-hidden !p-0 !flex !flex-col !gap-0 sm:!left-1/2 sm:!right-auto sm:!top-1/2 sm:!bottom-auto sm:!h-auto sm:!max-h-[88dvh] sm:!w-[28rem] sm:!max-w-[28rem] sm:!-translate-x-1/2 sm:!-translate-y-1/2 sm:!rounded-2xl"
         )}
         style={responsiveMobileSheet ? undefined : style}
         {...props}
